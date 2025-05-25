a058e4b2f3b64cbd44004a332b355dad34fd7aef
HornedCoder <kayush5u2018@gmail.com>
2025-05-25 18:39:55 +0530

    Completed Eleven

--- src/Eleven.js
+++ src/Eleven.js
@@ -1,25 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Eleven = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isRegistered, setIsRegistered] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const storedUsers = localStorage.getItem('users');
+        if (storedUsers) {
+            setUsers(JSON.parse(storedUsers));
+        }
+    }, []);
+
+    const handleAuthentication = (e) => {
+        e.preventDefault();
+        if(isRegistered){
+            //LogIN
+            const user = users.find((u) => u.email === email && u.password === password);
+            if (user){
+                setIsLoggedIn(true);
+            }
+            else{
+                alert("Please check credentials");
+            }
+        }
+        else{
+            //Register
+            const newUsers = {email, password};
+            setUsers([...users, newUsers]);
+            localStorage.setItem('users', JSON.stringify([...users, newUsers]));
+            setIsLoggedIn(true);
+        }
+    }
+
+    const handleLogout=() =>{
+        setIsLoggedIn(false);
+        setEmail('');
+        setPassword('');
+    }
+
     return (
         <div>
             {
                 isLoggedIn ? (
                     <div>
                         <h2>Welcome, {email}</h2>
-                        <button>Logout</button>
+                        <button onClick={handleLogout}>Logout</button>
                     </div>
                 ) : (
                     <div>
                         <h2>{isRegistered?"Login":"Register"}</h2>
-                        <form>
+                        <form onSubmit={handleAuthentication}>
                             <input type="email" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
                             <input type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                            <button>{isRegistered ? "Login" : "Register"}</button>
+                            <button type="submit">{isRegistered ? "Login" : "Register"}</button>
                         </form>
                         <p>
                             {isRegistered ? "Don't have an account Register Now!" : "Already have an account! Log In"}
